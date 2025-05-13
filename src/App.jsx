9b2923793e5c5e345d02284f07ca4677e01b641a
Valeria Pagliarini <valepagliar@gmail.com>
2025-05-13 16:07:13 +0200

    not complete

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import MyFooter from "./components/MyFooter";
+import MyNav from "./components/MyNav";
+import Welcome from "./components/Welcome.jsx";
+
+function App() {
+  return (
+    <>
+      <MyNav />
+      <Welcome />
+      <MyFooter />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,18 @@
+import { Alert, Col, Container, Row } from "react-bootstrap";
+
+const Welcome = function () {
+  return (
+    <Container>
+      <Row className="justify-content-center">
+        <Col xs={12} md={10}>
+          <div className="d-flex justify-content-center align-items-center gap-3">
+            <h1>Epibooks</h1>
+            <p className="m-0">Your Epic Bookstore!</p>
+          </div>
+        </Col>
+      </Row>
+      <Alert variant="warning">Scorri per vedere i nostri libri</Alert>
+    </Container>
+  );
+};
+export default Welcome;
