b15dd97ed1840bbd5c246f3c04fc4aa2c364ffbd
Valeria Pagliarini <valepagliar@gmail.com>
2025-05-13 16:59:09 +0200

    fixed cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import AllTheBooks from "./components/AllTheBooks.jsx";
 import MyFooter from "./components/MyFooter";
 import MyNav from "./components/MyNav";
 import Welcome from "./components/Welcome.jsx";
@@ -8,6 +9,7 @@ function App() {
     <>
       <MyNav />
       <Welcome />
+      <AllTheBooks />
       <MyFooter />
     </>
   );
--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -2,14 +2,15 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 
 const MyNav = function () {
   return (
-    <Navbar expand="md" className="bg-body-tertiary">
+    <Navbar expand="md" className="bg-body-tertiary topbar">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Epibooks</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#">Home</Nav.Link>
+            <Nav.Link href="#">About Us</Nav.Link>
+            <Nav.Link href="#">Brosw</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
